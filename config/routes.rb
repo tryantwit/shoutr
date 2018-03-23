0d0460d524391f8c95a6b7dfa45c05f2b1601c76
Tim Ryan <tryan@valcom.com>
2018-03-23 10:30:45 -0400

    Allow user to shout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   end
 
   root to: 'homes#show'
+  resources :shouts, only: [:create]
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
